11d9c682a216f64d43c6cc4b2920d6b4718ffa9f
AJ Agrawal <aj@alumnifyapp.com>
2013-12-12 07:17:22 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoAppThree::Application.config.secret_key_base = 'b33dd433a37e54d52ebce78c272d8ca71bbcbe485a6f92958667cd460bd74d44387b03216c76ec7b8c58bd5bf20fb308d2449bf3ea1d73072a07e257bcf56ee8'
